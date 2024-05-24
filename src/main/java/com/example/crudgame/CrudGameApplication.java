c84ea4f6a1e829878339eb696bb0a69d08c7e86e
grisella <sergobeast@gmail.com>
2024-05-24 20:20:09 +0300

    init

---
+++ src/main/java/com/example/crudgame/CrudGameApplication.java
@@ -0,0 +1,13 @@
+package com.example.crudgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudGameApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrudGameApplication.class, args);
+    }
+
+}
